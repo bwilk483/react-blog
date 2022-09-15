44e5713d6ff5bc2b972273e2269421a8bd0a06af
Bryan Wilkerson <bwilk483@gmail.com>
2022-09-15 16:44:28 -0500

    finished homepage header for now

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,56 @@
 import React from "react";
-
 import "./NavBar.css";
 
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+  FaLinkedin,
+  FaSearch,
+} from "react-icons/fa";
+
+const dev = "img/me.jpg";
+
 const NavBar = () => {
-  return <div className="NavBar">hello form NavBar</div>;
+  return (
+    <div className="navBar">
+      <div className="navLeft">
+        <FaFacebookSquare
+          size={20}
+          style={{ color: "#444", marginRight: "1rem", cursor: "pointer" }}
+        />
+        <FaTwitterSquare
+          size={20}
+          style={{ color: "#444", marginRight: "1rem", cursor: "pointer" }}
+        />
+        <FaLinkedin
+          size={20}
+          style={{ color: "#444", marginRight: "1rem", cursor: "pointer" }}
+        />
+        <FaInstagramSquare
+          size={20}
+          style={{ color: "#444", marginRight: "1rem", cursor: "pointer" }}
+        />
+      </div>
+
+      <div className="navCenter">
+        <ul className="navList">
+          <li className="navListItems">HOME</li>
+          <li className="navListItems">ABOUT</li>
+          <li className="navListItems">CONTACT</li>
+          <li className="navListItems">WRITE</li>
+          <li className="navListItems">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="navRight">
+        <img className="navImg" src={dev} alt="developer"></img>
+        <FaSearch
+          size={20}
+          style={{ color: "#666", marginLeft: "1rem", cursor: "pointer" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/NavBar/NavBar";
+import Header from "../components/Header/Header";
 
 const home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 };
