fd3ca5fe5b94e44bb4b7902f0ac99655236ef03e
Bryan Wilkerson <bwilk483@gmail.com>
2022-09-22 00:31:08 -0500

    finished server

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+// importing all the routes
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cd(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("./api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("3001", () => {
+  console.log("Backend is running.");
+});
---
+++ server/routes/categories.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+//Create new Category
+router.post("/", async (req, res) => {
+  const newCat = new Category(req.body);
+  try {
+    const savedCat = await newCat.save();
+    res.status(200).send(savedCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get Category
+router.get("/", async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).send(cats);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
